8c4d6e3c48a303a1eb4b4dabc12ddd1a3bfd1f97
Hussein Kasim <husseinkasim20@yahoo.com>
2025-08-29 22:44:58 +0200

    Partially updated linear regression gradient descent code
    Updated linear regression gradient descent code to include support for multiple features. Not fully functional as of yet.

--- custom_ml_library/linear_regression.py
+++ custom_ml_library/linear_regression.py
@@ -2,6 +2,11 @@ import numpy as np
 
 # Test data for development
 x = [3,4,6,8,9]
+x_two_features = [[1,2],
+                  [3,4],
+                  [5,6],
+                  [7,8],
+                  [9,10]]
 x_three_features = [[1, 4.55,35], 
                   [1.32, 6.54,44.4], 
                   [5.34, 8.79, 56.7], 
@@ -47,47 +52,82 @@ def linear_reg_normal(x, y):
 
 
 # Create a linear regression model using gradient descent
-def linear_reg_gradient(x, y, learning_rate, iterations=5, m=0, b=0):
+def linear_reg_gradient(x, y, learning_rate, iterations, m, b):
     """
     Computes predictions for linear regression using gradient descent
     x: Python list of values of the feature (only supports single features for now)
     y: Python list of actual values of target
     learning rate: the value of the learning rate used for recalculating m and b
-    iterations: the number of iterations to train the model for (default value = 5)
-    m: slope value (default value = 0)
-    b: intercept value (default value = 0)
+    iterations: the number of iterations to train the model for
+    m: Python list of slope values
+    b: intercept value
     returns NumPy array of predicted values of target
     """
      
     # Instantiate y_pred and error and initialize iteration counter
     y_pred = []
     error = []
+    dm_list = []
     interation_coutner = 0
 
     while(interation_coutner < iterations):
         # Clear predictions and errors
         y_pred.clear()
         error.clear()
-
+        dm_list.clear()
+        
         # Initialize dm and db
         dm = 0
-        db= 0
+        db = 0
         
-        for index, val_x in enumerate(x):
-            # Calculate y_pred
-            y_pred.append(m*x[index]+b)
+        for index, _ in enumerate(x):
+            # Multiple features
+            if isinstance(x[0], list):
+                '''  
+                # Initialize y_pred_temp
+                y_pred_temp = 0
 
-            # Calculate error
-            error.append(y[index] - y_pred[index])
+                # Calculate y_pred
+                for index2, _ in enumerate(x[index]):
+                    y_pred_temp += m[index2]*x[index][index2]
+                y_pred_temp+=b
+                y_pred.append(y_pred_temp)
 
-            # Calculate gradients
-            dm += 2/len(x) * x[index] * ((m*x[index]+b) - y[index])
-            db += 2/len(x) * ((m*x[index]+b) - y[index])
+                # Calculate error
+                error.append(y[index] - y_pred[index])
+
+                # Calculate gradient dm
+                for index2, _ in enumerate(x[index]):
+                    dm += 2/len(x) * x[index][index2] * (y_pred[index] - y[index])
+                dm_list.append(dm)
+                '''      
+            # Single feature
+            elif isinstance(x[0], int) or isinstance(x[0], float):
+                # Calculate y_pred
+                y_pred.append(m[0]*x[index]+b)
+
+                # Calculate error
+                error.append(y[index] - y_pred[index])
+
+                # Calculate gradient dm
+                dm += 2/len(x) * x[index] * (y_pred[index] - y[index])
+                dm_list.append(dm)
+            
+            # Calculate gradient db
+            db += 2/len(x) * (y_pred[index] - y[index])
 
             # For multiple features, the total dm values must be stored in a list since we will have an m value for each feature
 
-        # Update m and b
-        m = m - learning_rate * dm
+        if isinstance(x[0], list):
+            # Update m
+            for index, _ in enumerate(m):
+                m[index] = m[index] - learning_rate * dm_list[index]
+
+        elif isinstance(x[0], int) or isinstance(x[0], float):
+            # Update m
+            m[0] = m[0] - learning_rate * dm_list[0]
+        
+        # Update b
         b = b - learning_rate * db
 
         # Update iteration counter
@@ -96,3 +136,4 @@ def linear_reg_gradient(x, y, learning_rate, iterations=5, m=0, b=0):
     return np.array(y_pred)
 
 
+print(linear_reg_gradient(x_two_features, y, 0.001, 1000, [0, 0], 0))
