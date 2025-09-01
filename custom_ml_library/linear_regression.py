3bafb3c8e9bb0756a472d9d38ecda6fc6848b8a3
Hussein Kasim <husseinkasim20@yahoo.com>
2025-09-01 19:07:16 +0200

    Code Optimization
    Unified single and multiple feature code for linear regression using gradient descent function

--- custom_ml_library/linear_regression.py
+++ custom_ml_library/linear_regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # Test data for development
-x = [3,4,6,8,9]
+x = [[3],[4],[6],[8],[9]]
 x_two_features = [[1,2],
                   [3,4],
                   [5,6],
@@ -51,10 +51,10 @@ def linear_reg_normal(x, y):
     return y_pred
 
 
-# Create a linear regression model using gradient descent
+# Create a linear regression model using (batch) gradient descent 
 def linear_reg_gradient(x, y, learning_rate, iterations, m, b):
     """
-    Computes predictions for linear regression using gradient descent
+    Computes predictions for linear regression using (batch) gradient descent
     x: Python list of values of the feature (only supports single features for now)
     y: Python list of actual values of target
     learning rate: the value of the learning rate used for recalculating m and b
@@ -63,7 +63,7 @@ def linear_reg_gradient(x, y, learning_rate, iterations, m, b):
     b: intercept value
     returns NumPy array of predicted values of target
     """
-     
+
     # Instantiate y_pred and error and initialize iteration counter
     y_pred = []
     error = []
@@ -86,48 +86,28 @@ def linear_reg_gradient(x, y, learning_rate, iterations, m, b):
         
         # Multiple features
         for index, _ in enumerate(x):
-            if isinstance(x[0], list):
-                # Initialize y_pred_temp
-                y_pred_temp = 0
+            # Initialize y_pred_temp
+            y_pred_temp = 0
 
-                # Calculate y_pred
-                for index2, _ in enumerate(x[index]):
-                    y_pred_temp += m[index2]*x[index][index2]
-                y_pred_temp+=b
-                y_pred.append(y_pred_temp)
+            # Calculate y_pred
+            for index2, _ in enumerate(x[index]):
+                y_pred_temp += m[index2]*x[index][index2]
+            y_pred_temp+=b
+            y_pred.append(y_pred_temp)
 
-                # Calculate error
-                error.append(y[index] - y_pred[index])
+            # Calculate error
+            error.append(y[index] - y_pred[index])
 
-                # Calculate gradient dm
-                for index2, _ in enumerate(x[index]):
-                    dm_list[index2] += 2/len(x) * x[index][index2] * (y_pred[index] - y[index])  
-
-            # Single feature
-            elif isinstance(x[0], int) or isinstance(x[0], float):
-                # Calculate y_pred
-                y_pred.append(m[0]*x[index]+b)
-
-                # Calculate error
-                error.append(y[index] - y_pred[index])
-
-                # Calculate gradient dm
-                dm += 2/len(x) * x[index] * (y_pred[index] - y[index])
-                dm_list.append(dm)
+            # Calculate gradient dm
+            for index2, _ in enumerate(x[index]):
+                dm_list[index2] += 2/len(x) * x[index][index2] * (y_pred[index] - y[index])  
             
             # Calculate gradient db
             db += 2/len(x) * (y_pred[index] - y[index])
-
-            # For multiple features, the total dm values must be stored in a list since we will have an m value for each feature
-
-        if isinstance(x[0], list):
-            # Update m
-            for index, _ in enumerate(m):
-                m[index] = m[index] - learning_rate * dm_list[index]
-
-        elif isinstance(x[0], int) or isinstance(x[0], float):
-            # Update m
-            m[0] = m[0] - learning_rate * dm_list[0]
+        
+        # Update m
+        for index, _ in enumerate(m):
+            m[index] = m[index] - learning_rate * dm_list[index]
         
         # Update b
         b = b - learning_rate * db
@@ -137,5 +117,3 @@ def linear_reg_gradient(x, y, learning_rate, iterations, m, b):
 
     return np.array(y_pred)
 
-
-print(linear_reg_gradient(x_two_features, y, 0.001, 100, [0, 0], 0))
